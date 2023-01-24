3c8e108d08e8cfb93a53652075071c7284b9e926
end-cycle <2991205548@qq.com>
2023-01-24 22:24:37 +0800

    ✨ feat(ShowDialog): 新增点击复制url方法

--- src/components/global/ShowDialog.vue
+++ src/components/global/ShowDialog.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="background">
-    <el-dialog
-      title="生成的链接"
-      v-model="sendVisiable"
-      width="30%"
-      top="20vh"
-    >
-      <a :href="src" target="_blank">{{src}}</a>
+    <el-dialog title="生成的链接" v-model="sendVisiable" width="30%" top="20vh">
+      <a :href="src" target="_blank">{{ src }}</a>
       <template #footer>
         <span class="dialog-footer">
+          <el-button @click.stop="copyUrl(src)">复 制</el-button>
           <el-button @click.stop="unVisiable">取 消</el-button>
         </span>
       </template>
@@ -18,19 +14,29 @@
 
 <script>
 export default {
-    emits:['update:sendVisiable'],
-    props:['sendVisiable','src'],
-    methods: {
-        unVisiable(){
-            this.$emit("update:sendVisiable",false)
-        }
+  emits: ['update:sendVisiable'],
+  props: ['sendVisiable', 'src'],
+  methods: {
+    unVisiable() {
+      this.$emit("update:sendVisiable", false)
     },
+    copyUrl(url) {
+      let urlTmpNode = document.createElement('input');
+      urlTmpNode.value = url;
+      document.body.appendChild(urlTmpNode);
+      urlTmpNode.select();
+      document.execCommand("Copy");
+      urlTmpNode.remove();
+      this.unVisiable();
+      this.$message.success("复制成功");
+    }
+  },
 }
 </script>
 
 <style scoped>
-.background{
-    background: #fff;
-    z-index: 104;
+.background {
+  background: #fff;
+  z-index: 104;
 }
 </style>
